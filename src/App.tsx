c477de1f7d875ea25f7c85d261e32b80b471433c
radiscoding <cccooonnnrrraaaddd@gmail.com>
2025-12-19 19:33:18 -0500

    hex color generator

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 import Accordian from './components/accordian/Accordian';
+import HexColorGenerator from './components/HexColorGenerator/HexColorGenerator';
 
 function App() {
   return (
     <>
-      <Accordian />
+      {/* <Accordian /> */}
+      <HexColorGenerator />
     </>
   );
 }
