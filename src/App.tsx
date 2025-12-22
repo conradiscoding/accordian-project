17c8175a098e93577fa3e102e144885596aad199
radiscoding <cccooonnnrrraaaddd@gmail.com>
2025-12-22 16:55:19 -0500

    remove carosouel library and refactor code

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
       {/* <Accordian /> */}
       {/* <HexColorGenerator /> */}
       {/* <StarRater /> */}
-      <ImageSlider />
+      <ImageSlider url={'https://picsum.photos/v2/list'} page={1} limit={10} />
     </>
   );
 }
